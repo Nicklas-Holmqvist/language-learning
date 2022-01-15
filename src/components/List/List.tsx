c90da127f2c48949be416c1836188c0f65589150
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-01-15 15:57:48 +0100

    fixed buttons

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,3 +1,4 @@
+import { Button, Grid } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import useStyles from './styles';
 
@@ -5,13 +6,18 @@ const List = () => {
 
     const classes = useStyles()
 
-    const list = [{i:1,w:'Ett',b:false}, {i:2,w:'2',b:false}, {i:3,w:'3',b:false}, {i:4,w:'4',b:false}, {i:5,w:'5',b:false}, {i:6,w:'6',b:false}, {i:7,w:'7',b:false}, {i:8,w:'8',b:false}, {i:9,w:'9',b:false}, {i:10,w:'10',b:false}]
-    const listTwo = [{i:1,w:'One'}, {i:2,w:'2'}, {i:3,w:'3'}, {i:4,w:'4'}, {i:5,w:'5'}, {i:6,w:'6'}, {i:7,w:'7'}, {i:8,w:'8'}, {i:9,w:'9'}, {i:10,w:'10'}]
+    const [loaded, setLoaded] = useState(true)
+
+    const [list, setList] = useState([{i:1,w:'Ett',b:false,n:''}, {i:2,w:'2',b:false,n:''}, {i:3,w:'3',b:false,n:''}, {i:4,w:'4',b:false,n:''}, {i:5,w:'5',b:false,n:''}, {i:6,w:'6',b:false,n:''}, {i:7,w:'7',b:false,n:''}, {i:8,w:'8',b:false,n:''}, {i:9,w:'9',b:false,n:''}, {i:10,w:'10',b:false,n:''}])
+    const [listTwo, setListTwo] = useState([{i:1,w:'One',b:false,n:''}, {i:2,w:'2',b:false,n:''}, {i:3,w:'3',b:false,n:''}, {i:4,w:'4',b:false,n:''}, {i:5,w:'5',b:false,n:''}, {i:6,w:'6',b:false,n:''}, {i:7,w:'7',b:false,n:''}, {i:8,w:'8',b:false,n:''}, {i:9,w:'9',b:false,n:''}, {i:10,w:'10',b:false,n:''}])
 
     useEffect(() => {
-        let randomListOne = list.sort((a, b) => Math.random() - 0.5)
-        let randomListTwo = listTwo.sort((a, b) => Math.random() - 0.5)
-    }, [])
+        if(loaded === true) {
+            let randomListOne = list.sort((a, b) => Math.random() - 0.5)
+            let randomListTwo = listTwo.sort((a, b) => Math.random() - 0.5)
+            setLoaded(false)
+        }        
+    },[loaded])
 
     const [correctListOne, setCorrectedListOne] = useState<{id:any, word: any}[]>([])
     const [correctListTwo, setCorrectedListTwo] = useState<{id:any, word: any}[]>([])
@@ -25,11 +31,11 @@ const List = () => {
             id:id,
             word:word
         }
-        
         if(isListOne && !correctListOne.find(i => i.id === id)){   
-            setActive(id)         
+            setActive(id, isListOne)         
             setCorrectedListOne(item => item.concat(newItem))
         } else if (!isListOne && !correctListTwo.find(i => i.id === id)){
+            setActive(id, isListOne)         
             setCorrectedListTwo(item => item.concat(newItem))
         } else return
     }
@@ -40,39 +46,66 @@ const List = () => {
         }
     }
 
-    const setActive = (id:number) => {
-       console.log(id)
-       const index = list.findIndex((i) => i.i === Number(id)) 
-       list[index].b = true 
-       console.log(list)
+    const setActive = (id:number, isListOne:boolean) => {
+        if(isListOne){
+            const index = list.findIndex((i) => i.i === Number(id)) 
+            list[index].b = true
+            list[index].n = (correctListOne.length+1).toString()
+            setList(list)
+        } else {
+            const index = listTwo.findIndex((i) => i.i === Number(id)) 
+            listTwo[index].b = true
+            listTwo[index].n = (correctListTwo.length+1).toString()
+            setListTwo(listTwo)
+        }
     }
 
     const mapListOne = list.map((word) => 
-    <li onClick={handleChoice} className={word.b ? classes.active : ''} value='listOne' id={word.i.toString()} key={word.i}>{word.w}</li> )
+    <Grid container item direction='row' className={classes.word}>
+        {word.n !== '' && <p className={classes.choiceLeft}>{word.n}</p>}
+        <Button 
+            onClick={handleChoice} 
+            variant={word.b ? 'contained' : 'outlined'} 
+            className={word.b ? classes.active : ''} 
+            value='listOne' 
+            id={word.i.toString()} 
+            key={word.i}
+            >
+                {word.w}
+        </Button> 
+    </Grid>
+    )
 
     const mapListTwo = listTwo.map((word) => 
-    <li onClick={handleChoice} value='listTwo' id={word.i.toString()} key={word.i}>{word.w}</li> )
-
-    const mapUserOne = correctListOne.map((word) => 
-    <li onClick={handleChoice} value='listOne' id={word.id} key={word.id}>{word.word}</li> )
-
-    const mapUserTwo = correctListTwo.map((word) => 
-    <li onClick={handleChoice} value='listTwo' id={word.id} key={word.id}>{word.word}</li> )
+        <Grid container item direction='row' className={classes.word}>
+        <Button 
+            onClick={handleChoice} 
+            variant={word.b ? 'contained' : 'outlined'} 
+            className={word.b ? classes.active : ''} 
+            value='listTwo' 
+            id={word.i.toString()} 
+            key={word.i}
+            >
+                {word.w}
+        </Button> 
+        {word.n !== '' && <p className={classes.choiceRight}>{word.n}</p>}
+    </Grid>)
 
     return(
-        <div>
+        <Grid container direction='column' className={classes.container}>
             <p>Hejdå!</p>
             <button onClick={correctAnswers}>Rätta</button> 
-            <p>Svenska</p>
-            {mapListOne}
-            <p>Engelska</p>
-            {mapListTwo}
-            <h1>Listan</h1>
-            <p>Lista 1</p>
-            {mapUserOne}
-            <p>Lista 2</p>
-            {mapUserTwo}
-        </div>
+            <Grid container>
+                <Grid item>
+                    <p>Svenska</p>
+                    {mapListOne}
+                </Grid>
+                <Grid item>
+                    <p>Engelska</p>
+                    {mapListTwo}
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
--- src/components/List/styles.ts
+++ src/components/List/styles.ts
@@ -3,5 +3,22 @@ import { makeStyles } from '@mui/styles';
 export default makeStyles(() => ({
     active:{
         fontWeight:'bold'
-    }
+    },
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        maxWidth: '48rem',
+        width: '100%',
+    },
+    word: {
+        display: 'flex',
+        width: '10rem',
+        justifyContent: 'flex-end'
+    },
+    choiceLeft: {
+        paddingRight: '2rem'
+    },
+    choiceRight: {
+        paddingLeft: '2rem'
+    },
 }))
