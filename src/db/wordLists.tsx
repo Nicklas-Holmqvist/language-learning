2cb9403161fb893e29c26c9a45284851c3d5af86
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-01-18 21:25:31 +0100

    removed week 5

--- src/db/wordLists.tsx
+++ src/db/wordLists.tsx
@@ -33,20 +33,4 @@ export const weeks:any = [
     { id: 5, word: "it looks like", boolean: false, number: "",  correct: false },
     { id: 6, word: "back on", boolean: false, number: "",  correct: false },
   ],]},
-  {5:[[
-    { id: 1, word: "mun", boolean: false, number: "",  correct: false },
-    { id: 2, word: "tand", boolean: false, number: "",  correct: false },
-    { id: 3, word: "mage", boolean: false, number: "",  correct: false },
-    { id: 4, word: "svans", boolean: false, number: "",  correct: false },
-    { id: 5, word: "armbåge", boolean: false, number: "",  correct: false },
-    { id: 6, word: "axel", boolean: false, number: "",  correct: false },
-  ],
-  [
-    { id: 1, word: "mouth", boolean: false, number: "",  correct: false },
-    { id: 2, word: "tooth", boolean: false, number: "",  correct: false },
-    { id: 3, word: "stomach", boolean: false, number: "",  correct: false },
-    { id: 4, word: "tail", boolean: false, number: "",  correct: false },
-    { id: 5, word: "elbow", boolean: false, number: "",  correct: false },
-    { id: 6, word: "shoulder", boolean: false, number: "",  correct: false },
-  ],]},
 ];
